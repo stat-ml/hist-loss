67541053c60d021156d3ddf3bb28807147c6dd46
Tismoney <mokrov@frtk.ru>
2021-01-04 00:07:29 +0300

    Fix tests

--- tests/test_backward.py
+++ tests/test_backward.py
@@ -9,21 +9,25 @@ from histloss import (
 import pytest
 import torch
 
-@pytest.fixture(
-    scope="module",
-    params=[
-        "HistogramLoss",
-        "InvHistogramLoss",
-        "BiHistogramLoss",
-        "EarthMoverDistanceLoss",
-        "NLLLoss"
-    ],
-)
-def test_loss(request):
-    criterion_cls = request.params
-    criterion = criterion_cls()
+def _test_loss(criterion_cls):
+    criterion = eval(criterion_cls)()
     positive = torch.sigmoid(torch.randn(10, requires_grad=True))
     negative = torch.sigmoid(torch.randn(10, requires_grad=True))
     loss = criterion(positive, negative)
     loss.backward()
     return 0
+
+def test_HistogramLoss():
+    return _test_loss("HistogramLoss")
+
+def test_InvHistogramLoss():
+    return _test_loss("InvHistogramLoss")
+
+def test_BiHistogramLoss():
+    return _test_loss("BiHistogramLoss")
+
+def test_EarthMoverDistanceLoss():
+    return _test_loss("EarthMoverDistanceLoss")
+
+def test_NLLLoss():
+    return _test_loss("NLLLoss")
