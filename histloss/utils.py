ef075d7dd361667d57fa04ff9812db914e89e270
Tismoney <mokrov.ns@phystech.edu>
2021-01-18 20:41:48 +0300

    upd utils

--- histloss/utils.py
+++ histloss/utils.py
@@ -1,6 +1,6 @@
 import torch
 
-def triangular_histogram_with_linear_slope(input, t, delta):
+def triangular_histogram_with_linear_slope(inputs, t, delta):
     """
     Function that calculates a histogram from an article
     [Learning Deep Embeddings with Histogram Loss](https://arxiv.org/pdf/1611.00822.pdf)
@@ -9,19 +9,18 @@ def triangular_histogram_with_linear_slope(input, t, delta):
         t (Tensor): tensor that contains the nodes of the histogram
         delta (float): step in histogram
     """
-    input = input.view(-1)
-
+    inputs = inputs.view(-1)
     # first condition of the second equation of the paper
-    x = input.unsqueeze(0) - t.unsqueeze(1) + delta
+    x = inputs.unsqueeze(0) - t.unsqueeze(1) + delta
     m = torch.zeros_like(x)
     m[(0 <= x) & (x <= delta)] = 1
-    a = torch.sum(x * m, dim=1) / ( delta * len(input))
+    a = torch.sum(x * m, dim=1) / ( delta * len(inputs))
 
     # second condition of the second equation of the paper
-    x = t.unsqueeze(0) - input.unsqueeze(1) + delta
+    x = t.unsqueeze(0) - inputs.unsqueeze(1) + delta
     m = torch.zeros_like(x)
     m[(0 <= x) & (x <= delta)] = 1
-    b = torch.sum(x * m, dim=0) / ( delta * len(input))
+    b = torch.sum(x * m, dim=0) / ( delta * len(inputs))
 
     return torch.add(a, b)
 
@@ -34,4 +33,10 @@ def inv_cumsum(t, dim=0):
     flip_cumsum = torch.cumsum(torch.cumsum(flip_t, dim), dim)
     inv_cumsum = torch.flip(flip_cumsum, [dim])
     return inv_cumsum
-    
+
+def norm_min_max_distributuions(*distributuions):
+    max_ = max(torch.max(d.data) for d in distributuions)
+    min_ = min(torch.min(d.data) for d in distributuions)
+
+    norm_distributuions = ((d - min_) / (max_ - min_) for d in distributuions)
+    return norm_distributuions
