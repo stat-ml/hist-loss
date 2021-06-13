4183d64d7402c2b80ada393d25eb0d9a3fe158ca
Tismoney <mokrov@frtk.ru>
2021-06-13 21:08:17 +0300

    Clean code

--- tests/test_backward.py
+++ tests/test_backward.py
@@ -1,45 +1,42 @@
-from histloss import (
-    HistogramLoss,
-    InvHistogramLoss,
-    BiHistogramLoss,
-    EarthMoverDistanceLoss,
-    NLLLoss,
-    BinomialDevianceLoss,
-    ContinuousHistogramLoss
-)
-
-import pytest
+import histloss
 import torch
 
+
 def _test_loss(criterion_cls):
-    criterion = eval(criterion_cls)()
+    criterion = getattr(histloss, criterion_cls)()
     positive = torch.sigmoid(torch.randn(10, requires_grad=True))
     negative = torch.sigmoid(torch.randn(10, requires_grad=True))
     loss = criterion(positive, negative)
     loss.backward()
     return 0
 
+
 def test_HistogramLoss():
     return _test_loss("HistogramLoss")
+
 
 def test_InvHistogramLoss():
     return _test_loss("InvHistogramLoss")
 
+
 def test_BiHistogramLoss():
     return _test_loss("BiHistogramLoss")
+
 
 def test_EarthMoverDistanceLoss():
     return _test_loss("EarthMoverDistanceLoss")
 
+
 def test_NLLLoss():
     return _test_loss("NLLLoss")
+
 
 def test_BinomialDevianceLoss():
     return _test_loss("BinomialDevianceLoss")
 
+
 def test_ContinuousHistogramLoss():
-    criterion = ContinuousHistogramLoss()
-    criterion = ContinuousHistogramLoss()
+    criterion = histloss.ContinuousHistogramLoss()
     distance = torch.rand(30, requires_grad=True)
     similarity = torch.randint(low=0, high=3, size=(30,)).to(torch.float)
     loss = criterion(distance, similarity)
