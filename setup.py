f9abadb5ca9506ab3c8dc72c578b446b04b9620e
Tismoney <mokrov@frtk.ru>
2021-06-13 21:09:01 +0300

    Add long desc

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import os
 import setuptools
 
+from histloss import __version__
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-from histloss import __version__
 
 def load_requirements(filename):
     with open(os.path.join(PROJECT_ROOT, filename), "r") as f:
         return f.read().splitlines()
+
+
+with open('README.md', 'r') as f:
+    DESCRIPTION = f.read()
 
 setuptools.setup(
     name="hist-loss",
@@ -15,6 +20,8 @@ setuptools.setup(
     author="Maxim Panov and Nikita Mokrov and Roman Lisov",
     author_email="nikita.mokrov@skoltech.ru",
     description="Package with losses for distribution learning",
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
     url="https://github.com/stat-ml/histloss",
     packages=setuptools.find_packages(),
     license="Apache License 2.0",
